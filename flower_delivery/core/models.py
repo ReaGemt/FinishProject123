974a6159c0fc67fa745559a8df131c1d41dc3b2b
ReaGemt <zero2028@inbox.ru>
2024-10-16 16:23:52 +0300

    1

--- flower_delivery/core/models.py
+++ flower_delivery/core/models.py
@@ -39,3 +39,16 @@ class Review(models.Model):
     comment = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='products/')
+    rating = models.FloatField(default=0.0)
+
+    def __str__(self):
+        return self.name
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product_images/')
--- flower_delivery/core/urls.py
+++ flower_delivery/core/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+
 ]
