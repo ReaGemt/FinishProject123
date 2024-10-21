3781540a31c890d107cb230b92b454c3aadb0182
ReaGemt <155527397+ReaGemt@users.noreply.github.com>
2024-10-21 22:36:44 +0300

    123

--- flower_delivery/core/views.py
+++ flower_delivery/core/views.py
@@ -15,7 +15,8 @@ from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect
 from django.utils import translation
-
+from django.shortcuts import redirect, get_object_or_404
+from .models import Product, Cart, CartItem
 
 def product_list(request):
     category = request.GET.get('category')
@@ -29,12 +30,16 @@ def product_list(request):
 @login_required
 def add_to_cart(request, product_id):
     product = get_object_or_404(Product, id=product_id)
-    cart, _ = Cart.objects.get_or_create(user=request.user)
+
+    # Проверяем, есть ли корзина у пользователя
+    cart, created = Cart.objects.get_or_create(user=request.user)
+
+    # Проверяем, если товар уже в корзине, увеличиваем количество, иначе создаем новый элемент
     cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
     if not created:
         cart_item.quantity += 1
     cart_item.save()
-    messages.success(request, f'{product.name} добавлен в корзину.')
+
     return redirect('view_cart')
 
 
